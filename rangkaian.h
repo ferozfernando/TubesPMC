eff72078072c1e80413784a836c59bfed560b5b0
Dhanurangga Al Fadh <rangga.dhanu10@gmail.com>
2020-04-22 14:35:59 +0700

    Create rangkaian.h

---
+++ rangkaian.h
@@ -0,0 +1,7 @@
+double genSin(double A, double f, double phase, double DC, double time);
+double genSqr(double A, double f, double phase, double DC, double time);
+// double genTri(double A, double f, double phase, double DC, double time);
+void rangkaian1(double v[], double R, double C, double src, int i);
+void rangkaian2(double v[], double R, double C, double src, int i);
+void rangkaian3(double v1[],double R, double C1, double C2, double Src[], int i);
+void rangkaian4(double v2[],double R1, double R2, double C, double Src, double prevSrc, int i);
