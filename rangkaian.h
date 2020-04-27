3340192f7481b95754116016c5aad3fc85486476
Dhanurangga Al Fadh <rangga.dhanu10@gmail.com>
2020-04-27 15:08:10 +0700

    Update rangkaian.h

--- rangkaian.h
+++ rangkaian.h
@@ -5,3 +5,4 @@ void rangkaian1(double v[], double R, double C, double src, int i);
 void rangkaian2(double v[], double R, double C, double src, int i);
 void rangkaian3(double v1[],double R, double C1, double C2, double Src[], int i);
 void rangkaian4(double v2[],double R1, double R2, double C, double Src, double prevSrc, int i);
+void rangkaian8(double v1[], double v2[], double v3[], double R1, double R2, double C1, double C2, double src, int i);
