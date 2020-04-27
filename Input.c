4e60c0dede718e80289c93753c46cda994ef4931
Dhanurangga Al Fadh <rangga.dhanu10@gmail.com>
2020-04-27 15:07:51 +0700

    Update Input.c
    Sudah ditambahkan comment dan case rangkaian 8

--- Input.c
+++ Input.c
@@ -7,23 +7,24 @@
 #define T 0.0001
 
 int main(){
-    int current_menu;
-    int signal;
-    int current_output = 0;
-    char filename[100];
-    double temp;
-    double A;
-    double f;
-    double phase;
-    double DCoff;
-    double source;
-    double R1;
-    double R2;
-    double C1;
-    double C2;
-    double time;
-    double t;
-    double n;
+    //Deklarasi Variabel
+    int current_menu;       //Untuk menentukan menu saat ini
+    int signal;             //Untuk menentukan jenis sinyal masukan
+    int current_output = 0; //Untuk menentukan sinyal keluaran yang ingin diplot
+    char filename[100];     //
+    double temp;            //Menyimpan nilai tipe data double untuk sementara
+    double A;               //Menyimpan nilai amplituda sinyal input
+    double f;               //Menyimpan nilai frekuensi sinyal input
+    double phase;           //Menyimpan nilai fasa sinyal input
+    double DCoff;           //Menyimpan nilai offset dc sinyal input
+    double source;          //Menyimpan nilai sinyal input pada detik ke time
+    double R1;              //Menyimpan nilai resistor
+    double R2;              //Menyimpan nilai resistor
+    double C1;              //Menyimpan nilai kapasitor
+    double C2;              //Menyimpan nilai kapasitor
+    double time;            //Menyimpan nilai waktu untuk simulasi
+    double t;               //Menyimpan nilai lama rentang waktu simulasi
+    double n;               //Menyimpan nilai banyaknya iterasi
     double *v2o;
     double *v2;
     double *v3o;
@@ -34,10 +35,12 @@ int main(){
     double *vsig;
     
     do {
-        mainmenu();
-        circuitList();
-        system("python3 menu.py");
+        mainmenu();             //Menampilkan menu utama
+        circuitList();          //Menampilkan daftar rangkaian yang tersedia
+        system("python menu.py");  // Menampilkan gambar rangkaian yang ada di circuitList()
         scanf("%d", &current_menu);
+
+        //Inisialisasi
         vo      = (double *)malloc(sizeof(double));
         vsigo   = (double *)malloc(sizeof(double));
         v2o     = (double *)malloc(sizeof(double));
@@ -47,14 +50,35 @@ int main(){
         *v3o    = 0;
         *vsigo  = 0;
 
-        switch(current_menu)
+        switch(current_menu) //Switch case jenis rangkaian berdasarkan input yang dimasukkan user
         {
-            case 1:
+            case 1:     //Rangkaian 1
+
+                //Menentukan jenis sinyal masukan (1: constant, 2: sinusoid, 3: square)
                 do{
                 signList();
                 scanf("%d", &signal);
-                switch(signal)
-                {
+                
+                //Switch case untuk memproses sinyal output berdasarkan jenis sinyal masukan
+                switch(signal){
+                    case 1:
+                        printf("Amplitude value: ");
+                        scanf("%lf", &A);
+                        printf("Input R: ");
+                        scanf("%lf", &R1);
+                        printf("Input C: ");
+                        scanf("%lf", &C1);
+                        printf("\nInput duration (in s): ");
+                        scanf("%lf", &t);
+                        n = t/T;
+                        v = (double *)realloc(vo, sizeof(double)*(n));
+                        vsig = (double *)realloc(vsigo, sizeof(double)*(n));
+                        for(int i=0; i<n-1; i++){
+                            rangkaian1(v, R1, C1, A, i);
+                            vsig[i] = A;
+                        }
+                    break;
+
                     case 2:
                         InputAC(&A, &f, &phase, &DCoff);
                         printf("Input R: ");
@@ -73,24 +97,6 @@ int main(){
                             vsig[i] = source;
                             time += T;
                             source = genSin(A,f,phase,DCoff,time);
-                        }
-                    break;
-
-                    case 1:
-                        printf("Amplitude value: ");
-                        scanf("%lf", &A);
-                        printf("Input R: ");
-                        scanf("%lf", &R1);
-                        printf("Input C: ");
-                        scanf("%lf", &C1);
-                        printf("\nInput duration (in s): ");
-                        scanf("%lf", &t);
-                        n = t/T;
-                        v = (double *)realloc(vo, sizeof(double)*(n));
-                        vsig = (double *)realloc(vsigo, sizeof(double)*(n));
-                        for(int i=0; i<n-1; i++){
-                            rangkaian1(v, R1, C1, A, i);
-                            vsig[i] = A;
                         }
                     break;
 
@@ -120,6 +126,7 @@ int main(){
                 }
                 }while(signal<1 || signal >3);
 
+                //Menu menampilkan grafik output rangkaian 1
                 do {
                     printf("Choose output: \n");
                     printf("1. V1 \n");
@@ -128,6 +135,8 @@ int main(){
                     printf("0. Back\n");
                     printf("Your choice: ");
                     scanf("%d", &current_output);
+                    
+                    //Switch case jenis output yang ingin diplot
                     switch(current_output){
                         case 1:
                             printV0("graph.csv", v, n);
@@ -153,12 +162,15 @@ int main(){
                 } while (current_output != 0);
                 break;
 
-            case 2:
+            case 2:     //Rangkaian 2
+                
+                //Menentukan jenis sinyal masukan (1: constant, 2: sinusoid, 3: square)
                 do{
                 signList();
                 scanf("%d", &signal);
-                switch(signal)
-                {
+                
+                //Switch case untuk memproses sinyal output berdasarkan jenis sinyal masukan
+                switch(signal){
                     case 1:
                         printf("Amplitude value: ");
                         scanf("%lf", &A);
@@ -225,6 +237,7 @@ int main(){
                 }
                 }while(signal<1 || signal >3);
 
+                //Menu untuk menampilkan grafik output rangkaian 2
                 do {
                     printf("Choose output: \n");
                     printf("1. V1 \n");
@@ -259,12 +272,15 @@ int main(){
                 } while (current_output != 0);
                 break;
 
-            case 3:
+            case 3:     //Rangkaian 3
+                
+                //Menentukan jenis sinyal masukan (1: constant, 2: sinusoid, 3: square)
                 do{
                 signList();
                 scanf("%d", &signal);
-                switch(signal)
-                {
+
+                //Switch case untuk memproses sinyal output berdasarkan jenis sinyal masukan
+                switch(signal){
                     case 1:
                         printf("Amplitude value: ");
                         scanf("%lf", &A);
@@ -334,6 +350,8 @@ int main(){
                         break;
                 }
                 }while(signal < 1 || signal >3);
+                
+                //Menu untuk menampilkan grafik output rangkaian 3
                 do {
                     printf("Choose output: \n");
                     printf("1. V(C1) \n");
@@ -367,6 +385,161 @@ int main(){
 
                         case 5:
                             printIC0("graph.csv", v, C2, n);
+                            graph();
+                            break;
+
+                        case 0:
+                            break;
+
+                        default:
+                            printf("Invalid Input!\n");
+                            break;
+                    }
+                } while (current_output != 0);
+                break;
+
+            case 8:     //Rangkaian 8
+                //Menentukan jenis sinyal masukan (1: constant, 2: sinusoid, 3: square)
+                do{
+                signList();
+                scanf("%d", &signal);
+
+                //Switch case untuk memproses sinyal output berdasarkan jenis sinyal masukan
+                switch(signal){
+                    case 1:
+                        printf("Amplitude value: ");
+                        scanf("%lf", &A);
+                        printf("Input R1: ");
+                        scanf("%lf", &R1);
+                        printf("Input R2: ");
+                        scanf("%lf", &R2);
+                        printf("Input C1: ");
+                        scanf("%lf", &C1);
+                        printf("Input C2: ");
+                        scanf("%lf", &C2);
+                        printf("\nInput duration (in s): ");
+                        scanf("%lf", &t);
+                        n = t/T;
+                        vo = (double *)realloc(vo, sizeof(double)*(n));
+                        v2o = (double *)realloc(v2o, sizeof(double)*(n));
+                        v3o = (double *)realloc(v3o, sizeof(double)*(n));
+                        vsig = (double *)realloc(vsigo, sizeof(double)*(n));
+                        for(int i=0; i<n-1; i++){
+                            rangkaian8(vo, v2o, v3o, R1, R2, C1, C2, A, i);
+                            vsig[i] = A;
+                        }
+                    break;
+
+                    case 2:
+                        InputAC(&A, &f, &phase, &DCoff);
+                        printf("Input R1: ");
+                        scanf("%lf", &R1);
+                        printf("Input R2: ");
+                        scanf("%lf", &R2);
+                        printf("Input C1: ");
+                        scanf("%lf", &C1);
+                        printf("Input C2: ");
+                        scanf("%lf", &C2);
+                        printf("\nInput duration (in s): ");
+                        scanf("%lf", &t);
+                        n = t/T;
+                        vo = (double *)realloc(vo, sizeof(double)*(n));
+                        v2o = (double *)realloc(v2o, sizeof(double)*(n));
+                        v3o = (double *)realloc(v3o, sizeof(double)*(n));
+                        vsig = (double *)realloc(vsigo, sizeof(double)*(n));
+                        time = 0;
+                        source = genSin(A,f,phase,DCoff,time);
+                        for(int i=0; i<n-1; i++){
+                            rangkaian8(vo, v2o, v3o, R1, R2, C1, C2, source, i);
+                            vsig[i] = source;
+                            time += T;
+                            source = genSin(A,f,phase,DCoff,time);
+                        }
+                    break;
+
+                    case 3:
+                        InputAC(&A, &f, &phase, &DCoff);
+                        scanf("%lf", &R1);
+                        printf("Input R2: ");
+                        scanf("%lf", &R2);
+                        printf("Input C1: ");
+                        scanf("%lf", &C1);
+                        printf("Input C2: ");
+                        scanf("%lf", &C2);
+                        printf("\nInput duration (in s): ");
+                        scanf("%lf", &t);
+                        n = t/T;
+                        vo = (double *)realloc(vo, sizeof(double)*(n));
+                        v2o = (double *)realloc(v2o, sizeof(double)*(n));
+                        v3o = (double *)realloc(v3o, sizeof(double)*(n));
+                        vsig = (double *)realloc(vsigo, sizeof(double)*(n));
+                        time = 0;
+                        source = genSqr(A,f,phase,DCoff,time);
+                        for(int i=0; i<n-1; i++){
+                            rangkaian8(vo, v2o, v3o, R1, R2, C1, C2, source, i);
+                            vsig[i] = source;
+                            time += T;
+                            source = genSqr(A,f,phase,DCoff,time);
+                        }
+                    break;
+
+                    default:
+                        printf("invalid input!\n");
+                }
+                }while(signal<1 || signal >3);
+
+                                //Menu untuk menampilkan grafik output rangkaian 3
+                do {
+                    printf("Choose output: \n");
+                    printf("1. V(R1) \n");
+                    printf("2. V(R2) \n");
+                    printf("3. V(C1) \n");
+                    printf("4. V(C2) \n");
+                    printf("5. I(R1) \n");
+                    printf("6. I(R2) \n");
+                    printf("7. I(C1) \n");
+                    printf("8. I(C2) \n");
+                    printf("0. Back\n");
+                    printf("Your choice: ");
+                    scanf("%d", &current_output);
+                    switch(current_output){
+                        case 1:
+                            printV("graph.csv", vo, v2o, n);
+                            graph();
+                            break;
+
+                        case 2:
+                            printV0("graph.csv", v3o, n);
+                            graph();
+                            break;
+
+                        case 3:
+                            printV("graph.csv", v2o, v3o, n);
+                            graph();
+                            break;
+
+                        case 4:
+                            printV0("graph.csv", v3o, n);
+                            graph();
+                            break;
+
+                        case 5:
+                            printIR("graph.csv", vo, v2o, R1, n);
+                            graph();
+                            break;
+
+                        case 6:
+                            printIR0("graph.csv", v3o, R2, n);
+                            graph();
+                            break;
+
+                        case 7:
+                            printIC("graph.csv", v2o, v3o, C1, n);
+                            graph();
+                            break;
+
+                        case 8:
+                            printIC0("graph.csv", v3o, C2, n);
                             graph();
                             break;
 
