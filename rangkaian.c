52a6891c57926c1ede213ea59689f8f3a85da003
Dhanurangga Al Fadh <rangga.dhanu10@gmail.com>
2020-04-28 15:21:59 +0700

    Rangkaian 1,2,3,8 sudah jadi tinggal dites. Rangkaian 4 masih salah buat sumber arus

--- rangkaian.c
+++ rangkaian.c
@@ -50,10 +50,52 @@ void rangkaian3V(double v1[], double v2[], double R, double C1, double C2, doubl
 	v1[i+1]	  = (I2-I1+(1/R+G1)*v2[i+1])/(1/R+G1+G2);
 }
 
-void rangkaian4(double v2[],double R1, double R2, double C, double Src, int i){
+void rangkaian4(double v2[],double R1, double R2, double C, double Src[], int i){
 	double Ga = C/T;
-	double Ia = C*(Src-v2[i])/T;
-	v2[i+1]	  = (Src*(Ga+(1/R1))-Ia)/(Ga+(1/R1)+(1/R2));
+	double Ia;
+	if (i > 1){
+        Ia = C*(Src[i-1]-v2[i])/T;
+	}
+	else{
+        Ia = C*(Src[i]-v2[i])/T;
+	}
+	v2[i+1]	  = (Src[i]*(Ga+(1/R1))-Ia)/(Ga+(1/R1)+(1/R2));
+}
+
+void rangkaian4I(double v1[], double v2[],double R1, double R2, double C, double Is, int i){
+	double Ga = C/T;
+	double Ia = C*(v2[i]-v1[i])/T;
+	v2[i+1]	  = Is*R2;
+	v1[i+1]	  = ((Is+Ia)/(1/R1+Ga))+v2[i+1];
+	printf("%lf, %lf, %lf\n", Is, v2[i], v1[i]);
+}
+
+void rangkaian5(double v2[],double R1, double R2, double C1, double C2, double Src[], int i){
+	double Ga = C1/T;
+	double Gb = C2/T;
+	double G1 = 1/R1;
+	double G2 = 1/R2;
+	double Ia = Ga*(Src[i+1]-v2[i+1]);
+	double Ib = Gb*(v2[i+1]);
+	v2[i+1]   = (Src[i]*(Ga+G1)+Ib-Ia)/(Ga+Gb+G1+G2);
+}
+
+void rangkaian6(double v2[],double v3[],double R1, double R2, double C1, double C2, double Src[], int i){
+	double Ga = C1/T;
+	double Gb = C2/T;
+	double Ia = Ga*(Src[i]-v2[i]);
+	double Ib = Gb*(v3[i]);
+	v2[i+1] = ((Src[i]*Ga)-Ia+(Ib/(1+(Gb*R2))))/(Ga+(Gb/(1+Gb*R2))+(1/R1));
+	v3[i+1] = (v2[i+1]+(Ib*R2))/(1+(R2*Gb));
+}
+
+void rangkaian7(double v2[],double v3[],double R1, double R2, double C1, double C2, double Src[], int i){
+	double Ga = C1/T;
+	double Gb = C2/T;
+	double Ia = Ga*v3[i];
+	double Ib = Gb*v2[i];
+	v2[i+1] = ((Src[i]/R1)+Ib+(Ia/(1+(Ga*R2))))/(Gb+(Ga/(1+Ga*R2))+(1/R1));
+	v3[i+1] = (v2[i+1]+(Ia*R2))/(1+(Ga*R2));
 }
 
 void rangkaian8(double v1[], double v2[], double v3[], double R1, double R2, double C1, double C2, double src, int i){
@@ -75,5 +117,4 @@ void rangkaian8V(double v1[], double v2[], double v3[], double R1, double R2, do
 	v1[i+1] = src;
 	v2[i+1] = (temp*(i2-i1)+i1+(v1[i+1]/R1))/(g1+(1/R1)-g1*temp);
 	v3[i+1] = (i1+(v1[i+1]/R1)-(v2[i+1]*(1/R1+g1)))/(-g1);
-	printf("%lf\n", v1[i+1]);
 }
