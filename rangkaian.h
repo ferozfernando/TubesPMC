dcf6d6730de9389f7f33641e93dadc6356d66788
sarahalyaat <alya.sarah.rara@gmail.com>
2020-04-29 16:05:19 +0700

    Update rangkaian.h

--- rangkaian.h
+++ rangkaian.h
@@ -10,6 +10,8 @@ void rangkaian4I(double v1[],double v2[],double R1, double R2, double C, double 
 void rangkaian5(double v2[],double R1, double R2, double C1, double C2, double Src[], int i);
 void rangkaian5I(double v1[],double v2[],double R1, double R2, double C1, double C2, double Is, int i);
 void rangkaian6(double v2[],double v3[],double R1, double R2, double C1, double C2, double Src[], int i);
+void rangkaian6I(double v1[],double v2[],double v3[],double R1, double R2, double C1, double C2, double Src[], int i);
 void rangkaian7(double v2[],double v3[],double R1, double R2, double C1, double C2, double Src[], int i);
+void rangkaian7I(double v1[], double v2[],double v3[],double R1, double R2, double C1, double C2, double Src[], int i);
 void rangkaian8(double v1[], double v2[], double v3[], double R1, double R2, double C1, double C2, double src, int i);
 void rangkaian8V(double v1[], double v2[], double v3[], double R1, double R2, double C1, double C2, double src, int i);
