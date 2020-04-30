daa1882916c7d6a185090cd1f0fe2ddce5862e2a
sarahalyaat <alya.sarah.rara@gmail.com>
2020-04-30 22:41:02 +0700

    Benerin gambar

--- menu.py
+++ menu.py
@@ -1,11 +1,7 @@
 import matplotlib.pyplot as plt
 
 # read the image
-im = plt.imread("pic.png")
-im1 = plt.imread("rangkaian_1.png")
-im2 = plt.imread("rangkaian_2.png")
+im = plt.imread("menu.png")
 # show the image
-plt.imshow(im1)
-plt.show()
-plt.imshow(im2)
+plt.imshow(im)
 plt.show()
